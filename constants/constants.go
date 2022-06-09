1db30ca4e7b8837c6bd5cf2bffc8d46b566b8281
Alfonso HH <alfonxd@gmail.com>
2022-06-09 22:42:23 +0200

    New intervals

--- constants/constants.go
+++ constants/constants.go
@@ -5,10 +5,10 @@ import (
 )
 
 const FetchVoiceDataInterval time.Duration = time.Duration(10) * time.Second
-const ProcessVoiceDataInterval time.Duration = time.Duration(60) * time.Second
+const ProcessVoiceDataInterval time.Duration = time.Duration(480) * time.Second
 
-const FetchMessageDataInterval time.Duration = time.Duration(60) * time.Second
-const ProcessMessageDataInterval time.Duration = time.Duration(240) * time.Second
+const FetchMessageDataInterval time.Duration = time.Duration(240) * time.Second
+const ProcessMessageDataInterval time.Duration = time.Duration(480) * time.Second
 
 const FetchNicknamesInterval time.Duration = time.Duration(240) * time.Second
 const FetchChannelNamesInterval time.Duration = time.Duration(240) * time.Second
