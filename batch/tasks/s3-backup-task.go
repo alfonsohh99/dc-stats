f5ee1e5ab2f1ea545d00701c7e0cfecab2e21ec5
Alfonso HH <alfonxd@gmail.com>
2022-06-12 20:12:38 +0200

    Rework backup structure

--- batch/tasks/s3-backup-task.go
+++ batch/tasks/s3-backup-task.go
@@ -21,48 +21,45 @@ import (
 func S3BackupTask(ctx context.Context, wait *sync.WaitGroup) {
 	defer wait.Done()
 
-	var guildList []model.Guild
-
-	guildList = []model.Guild{}
-
 	cursor, err := database.DataCollection.Find(ctx, bson.D{})
 
 	if err != nil {
 		log.Println("ERROR FETCHING DATA GUILDS", err)
 	}
 
-	for next := cursor.Next(ctx); next; next = cursor.Next(ctx) {
-		guildItem := model.Guild{}
-		cursor.Decode(&guildItem)
-		guildList = append(guildList, guildItem)
-	}
-
-	guildListString, err := json.Marshal(guildList)
-
-	if err != nil {
-		log.Println("Error JSON parsing guildList", err)
-		return
-	}
-
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		log.Println("Error creating AWS config", err)
+		log.Println("Error configurating AWS client: ", err)
 	}
-
 	client := s3.NewFromConfig(cfg)
 
 	uploader := manager.NewUploader(client)
-	dateNow := time.Now()
-	_, err = uploader.Upload(ctx, &s3.PutObjectInput{
-		Bucket: aws.String(botConfig.S3Bucket),
-		Key:    aws.String("backups/" + dateNow.Format("2006-01") + "/" + dateNow.Format("2006-01-06 15:04:05")),
-		Body:   strings.NewReader(string(guildListString)),
-	})
 
-	if err != nil {
-		log.Println("ERROR UPLOADING BACKUP")
-	} else {
-		log.Println("BACKUP UPLOADED CORRECTLY")
+	dateNow := time.Now()
+
+	for next := cursor.Next(ctx); next; next = cursor.Next(ctx) {
+		guildItem := model.Guild{}
+		cursor.Decode(&guildItem)
+
+		guildString, err := json.Marshal(guildItem)
+
+		if err != nil {
+			log.Println("Error JSON parsing guild: ", err)
+			return
+		}
+
+		_, err = uploader.Upload(ctx, &s3.PutObjectInput{
+			Bucket: aws.String(botConfig.S3Bucket),
+			Key:    aws.String("backups/" + guildItem.GuildID + "/" + dateNow.Format("2006-01") + "/" + dateNow.Format("2006-01-06 15:04:05")),
+			Body:   strings.NewReader(string(guildString)),
+		})
+
+		if err != nil {
+			log.Println("ERROR UPLOADING BACKUP, ", guildItem.GuildID)
+		}
+
 	}
+
+	log.Println("Finished backing up all guilds")
 
 }
